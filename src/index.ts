99c313473e924645274a4c8ad8fc39088218f667
unknown <dannylwe11@gmail.com>
2021-01-23 12:05:23 +0300

    add nodemon, concurrently and index.ts: - npm i nodemon -D - npm i concurrently -D

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world")
+import express from 'express'
+
+const app = express();
+const PORT = process.env.PORT || 9000
+app.listen(PORT, () => {
+    console.log("server is running on port %d", PORT)
+})
