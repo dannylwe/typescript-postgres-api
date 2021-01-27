b2f22125d760253868a71df449b510b27ee88259
unknown <dannylwe11@gmail.com>
2021-01-27 12:15:15 +0300

    added CRUD functionality

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -11,3 +11,67 @@ export const getUsers = async (req: Request, res:Response): Promise<Response> =>
         return res.status(500).json('Internal server Error');
     }
 }
+
+export const getUserById = async (req: Request, res:Response): Promise<Response> => {
+    try {
+        const ID: Number = parseInt(req.params.id)
+        const results: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [ID]);
+        return res.status(200).json(results.rows);
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json('Internal server Error');
+    }
+}
+
+export const createUser = async (req: Request, res:Response): Promise<Response> => {
+    const { name, email } = req.body
+    if (name == "" || email == "") {
+        return res.status(400).json('Invalid data');
+    }
+
+    try {
+        const results: QueryResult = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+        return res.status(201).json({
+            message: 'User created',
+            user : {
+                name,
+                email
+            }
+        });
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json('Internal server Error');
+    }
+}
+
+export const updateUser = async (req: Request, res:Response): Promise<Response> => {
+    try {
+        const ID: Number = parseInt(req.params.id)
+        const { name, email } = req.body
+        if (name == "" || email == "") {
+            return res.status(400).json('Invalid data');
+        }
+        const results: QueryResult = await pool.query('UPDATE users set name = $1, email = $2 WHERE id = $3', [name, email, ID]);
+        return res.status(200).json({
+            message: `user deleted ${ID}`,
+            user : {
+                name,
+                email
+            }
+        });
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json('Internal server Error');
+    }
+}
+
+export const deleteUser = async (req: Request, res:Response): Promise<Response> => {
+    try {
+        const ID: Number = parseInt(req.params.id)
+        const results: QueryResult = await pool.query('DELETE FROM users WHERE id = $1', [ID]);
+        return res.status(200).json(`user deleted ${ID}`);
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json('Internal server Error');
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { getUsers } from '../controllers/usersController'
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/usersController'
 
 const router = Router();
 router.get('/health', (req, res) => res.send('api is healthy'));
 router.get('/users', getUsers);
-// router.post('/users', getUsers);
-// router.get('/users/:id', getUsers);
-// router.put('/users/:id', getUsers);
-// router.delete('/users/:id', getUsers);
+router.post('/users', createUser);
+router.get('/users/:id', getUserById);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
