f8fd257aa36dcc1c59bdde3fa38abafe65558466
unknown <dannylwe11@gmail.com>
2021-01-27 13:54:16 +0300

    add type checking in user controller

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { pool } from '../database';
 import { QueryResult } from 'pg';
+import { Iuser } from '../modules/user'
 
 export const getUsers = async (req: Request, res:Response): Promise<Response> => {
     try {
-        const results: QueryResult = await pool.query('SELECT * FROM users');
+        const results: QueryResult<Iuser[]> = await pool.query('SELECT * FROM users');
         return res.status(200).json(results.rows);
     } catch(e) {
         console.log(e)
@@ -15,7 +16,7 @@ export const getUsers = async (req: Request, res:Response): Promise<Response> =>
 export const getUserById = async (req: Request, res:Response): Promise<Response> => {
     try {
         const ID: Number = parseInt(req.params.id)
-        const results: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [ID]);
+        const results: QueryResult<Iuser> = await pool.query('SELECT * FROM users WHERE id = $1', [ID]);
         return res.status(200).json(results.rows);
     } catch(e) {
         console.log(e)
@@ -28,15 +29,16 @@ export const createUser = async (req: Request, res:Response): Promise<Response> 
     if (name == "" || email == "") {
         return res.status(400).json('Invalid data');
     }
+    const user: Iuser = {
+        name,
+        email
+    }
 
     try {
-        const results: QueryResult = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+        const results: QueryResult = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [user.name, user.email]);
         return res.status(201).json({
             message: 'User created',
-            user : {
-                name,
-                email
-            }
+            user
         });
     } catch(e) {
         console.log(e)
@@ -48,16 +50,17 @@ export const updateUser = async (req: Request, res:Response): Promise<Response> 
     try {
         const ID: Number = parseInt(req.params.id)
         const { name, email } = req.body
-        if (name == "" || email == "") {
+        const user: Iuser = {
+            name,
+            email
+        }
+        if (user.name == "" || user.email == "") {
             return res.status(400).json('Invalid data');
         }
-        const results: QueryResult = await pool.query('UPDATE users set name = $1, email = $2 WHERE id = $3', [name, email, ID]);
+        const results: QueryResult = await pool.query('UPDATE users set name = $1, email = $2 WHERE id = $3', [user.name, user.email, ID]);
         return res.status(200).json({
             message: `user deleted ${ID}`,
-            user : {
-                name,
-                email
-            }
+            user
         });
     } catch(e) {
         console.log(e)
