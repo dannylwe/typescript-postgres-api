61be84769ae473fab30df82f67d4b8188f67555c
unknown <dannylwe11@gmail.com>
2021-01-27 11:21:44 +0300

    implement get all users endpoint

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,13 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { pool } from '../database';
+import { QueryResult } from 'pg';
 
-export const getUsers = (req: Request, res:Response) => {
-    res.send('users');
+export const getUsers = async (req: Request, res:Response): Promise<Response> => {
+    try {
+        const results: QueryResult = await pool.query('SELECT * FROM users');
+        return res.status(200).json(results.rows);
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json('Internal server Error');
+    }
 }
