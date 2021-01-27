032cd5673f5029f683bf1223e19ad7d8ea7516bd
unknown <dannylwe11@gmail.com>
2021-01-27 10:55:17 +0300

    setup basic routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express'
+import { getUsers } from '../controllers/usersController'
 
 const router = Router();
 router.get('/health', (req, res) => res.send('api is healthy'));
+router.get('/users', getUsers);
+// router.post('/users', getUsers);
+// router.get('/users/:id', getUsers);
+// router.put('/users/:id', getUsers);
+// router.delete('/users/:id', getUsers);
 
 export default router;
